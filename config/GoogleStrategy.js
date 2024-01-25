33f0a9186eed743518e1613f49bb74feff7a1ab5
harshlathiya91@gmail.com <harshlathiya@gmail.com>
2024-01-25 13:20:57 +0530

    six  commit

---
+++ config/GoogleStrategy.js
@@ -0,0 +1,39 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const passport = require('passport');
+const user = require('../models/user');
+const bcrypt = require('bcrypt');
+
+
+passport.use(new GoogleStrategy({
+    clientID: '1035647780616-iioi7ddkv1siej894gi9nanadgmm60jh.apps.googleusercontent.com',
+    clientSecret: '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',
+    callbackURL: "/google/callback"
+  },
+  async function(accessToken, refreshToken, profile, cb) {
+    //console.log(profile);
+    let checkemail = await user.findOne({ email: profile.emails[0].value });
+    if (checkemail) {
+       cb(null,checkemail);
+    }
+    else{
+        var pass = `${profile.emails[0].value}@123`;
+        let userdetails ={
+            username: profile.displayName,
+            email: profile.emails[0].value,
+            password : await bcrypt.hash(pass,10),
+            role:'user'
+        }
+        let userdatanew =await user.create(userdetails);
+        if(userdatanew){
+            return cb(null,userdatanew);
+        }
+        else{
+            return cb(null,false);
+        }
+    }
+  
+  }
+));
+
+
+module.exports = GoogleStrategy;
